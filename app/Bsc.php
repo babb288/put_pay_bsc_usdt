2d31483b663827f90350f9314f813c46be0d7574
bbab <bbab28@qq.com>
2025-12-25 17:51:33 +0800

    00

--- app/Bsc.php
+++ app/Bsc.php
@@ -439,7 +439,9 @@ class Bsc
         // 确保amounts数组中的值都转换为wei格式（18位小数）的字符串
         $amountsFormatted = [];
         foreach ($amounts as $amount) {
-            $amount = $amount - 0.00001;
+            $result = strstr($amount, '.', true) ?: $amount;
+            var_dump($result);
+            exit();
             $amountWei = $this->toBnbHex((string)$amount);
             $amountsFormatted[] = $amountWei;
         }
