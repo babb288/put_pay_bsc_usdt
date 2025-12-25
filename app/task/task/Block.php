63912ea0292b6145ca1cf9d134965cda029b72fe
bbab <bbab28@qq.com>
2025-12-25 17:34:34 +0800

    00

--- app/task/task/Block.php
+++ app/task/task/Block.php
@@ -60,7 +60,7 @@ class Block
                     continue;
                 }
 
-                $transfer['amount'] = $this->evmTransferScanner->formatTokenAmount($transfer['value'], 18);
+                $transfer['amount'] = $this->evmTransferScanner->formatTokenAmount($transfer['value'], 2);
                 $this->handle($transfer);
             }
 
